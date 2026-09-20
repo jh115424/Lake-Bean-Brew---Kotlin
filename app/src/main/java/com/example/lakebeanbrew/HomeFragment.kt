9f5e9cf116e1ebdc8d3d1e4b6df76afc19821cf1
Jenny Hodge <jenny@JennysMcBookNeo.lan>
2026-09-20 17:58:30 -0500

    connecting homePage buttons

--- app/src/main/java/com/example/lakebeanbrew/HomeFragment.kt
+++ app/src/main/java/com/example/lakebeanbrew/HomeFragment.kt
@@ -1,6 +1,23 @@
 package com.example.lakebeanbrew
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val order = view.findViewById<TextView>(R.id.img_order)
+
+
+
+order.setOnClickListener {
+
+}
+    }
+
+
 }
